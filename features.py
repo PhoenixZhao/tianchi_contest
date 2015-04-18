6a9c9b3f31b93ba2991e7a1d09ceb4e2ecf4da32
ZhaoHuan <byzhaohuan@gmail.com>
2015-04-18 12:50:35 +0800

    fix lc_date_delta calculationg bug

--- features.py
+++ features.py
@@ -88,7 +88,8 @@ class FeatureGenerator(object):
         states_data = []
         pos, neg, feature_num = 0, 0, 0
         for ui, m_features in mon_nums.items():
-            one_states_data = [ui]
+            uid, item_id = ui.split('-')
+            one_states_data = [int(uid), int(item_id)]
             one_states_data.extend(m_features)
             one_states_data.extend(l3d_nums.get(ui,[0,0,0,0,0]))
 
@@ -96,7 +97,10 @@ class FeatureGenerator(object):
             #one_states_data.append(item_l3d_clicks.get(item_id, 0))
 
             ui_lc_date = datetime.strptime(lc_date_str[ui], "%Y-%m-%d %H")#数据库的时间类型为"2014-12-18 8"
-            lc_delta = self.split_date - ui_lc_date
+            #datetime的类型计算天数差值不准确，必须转换为date类型
+            lc_delta = self.split_date.date() - ui_lc_date.date()
+            if lc_delta.days == 0:
+                logging.info('lc_delta days is 0, ui=%s, split_date=%s, ui_lc_date=%s', ui, self.split_date_str, lc_date_str[ui])
             one_states_data.append(lc_delta.days)
 
             #one_states_data.append(labels[ui])
@@ -106,7 +110,7 @@ class FeatureGenerator(object):
             else:
                 neg += 1
             if not feature_num:
-                feature_num = len(one_states_data) - 1
+                feature_num = len(one_states_data) - 2
             states_data.append(one_states_data)
 
         logging.info('merged all features,feature_num=%s; samples: pos=%s neg=%s', feature_num, pos, neg)
@@ -130,7 +134,7 @@ class FeatureGenerator(object):
 
 if __name__ == '__main__':
     dal = RECDAL()
-    split_date_str = '2014-12-18'
-    table_name = 'split_20141218_states'
+    split_date_str = '2014-12-17'
+    table_name = 'split_20141217_stats'
     generator = FeatureGenerator(dal, split_date_str, table_name)
     generator.generate_features()
